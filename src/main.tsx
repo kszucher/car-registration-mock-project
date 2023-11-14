27b55db025db7b8868e649d5fe739d00558cd728
Krisztian Szucher <krisztian@szucher.com>
2023-11-14 11:20:47 +0100

    reorg

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import CarRegistration from './components/CarRegistration.tsx'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <Theme appearance="dark" accentColor="jade" grayColor="sage">
-        <App/>
+        <CarRegistration/>
       </Theme>
     </Provider>
   </React.StrictMode>,
